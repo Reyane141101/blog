6e2ed9e69fb0f35dbac6946b960f9c1f9e36c840
Reyane En-nabty <reyane.en-nabty@epita.fr>
2024-09-29 12:35:26 +0100

    push

---
+++ back_end/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cardData = require('./cardData');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/cardData', (req, res) => {
+    res.json({
+        message: "Here is the card to display on the site",
+        data: cardData
+    });
+});
+
+
+const hostname = "127.0.0.1";
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Backend running on http://${hostname}:${port}`);
+});
